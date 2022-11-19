a5c0429e74e2d5b78daaa120f3fb19568f6450da
lucasmanfioletti <lucas.manfioletti@conversica.com>
2022-11-18 21:29:57 -0300

    init comit w/ create react app clean

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeDriverPage from './features/homeDriver/HomeDriverPage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeDriverPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/homeDriver/HomeDriverPage.jsx
@@ -0,0 +1,8 @@
+const HomeDriverPage = () => {
+
+    return <h1>This is the Home Driver Page</h1>
+
+};
+
+
+export default HomeDriverPage;
