7d1c96459ee8a3ac1b34d4256d6c12b2c606eacd
Lucas Manfioletti <lucas.manfioletti@conversica.com>
2023-01-14 18:14:11 -0300

    changes on sidebar and card for graphics

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Box, Stack, Text, Link, Icon } from '@chakra-ui/react';
-import { RiDashboardLine } from "react-icons/ri";
+import { RiDashboardLine, RiQuestionLine } from "react-icons/ri"; 
 
 const Sidebar = () => {
     return (
@@ -11,6 +11,15 @@ const Sidebar = () => {
                         <Link display="flex" alignItems="center">
                             <Icon as={RiDashboardLine} fontSize="20" />
                             <Text ml={4} fontWeight="medium">Dashboard</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+                <Box>
+                <Text fontWeight="bold" color="gray.400" fontSize="small">CONTATO</Text>
+                    <Stack spacing={4} mt="6" align="stretch">
+                        <Link display="flex" alignItems="center">
+                            <Icon as={RiQuestionLine} fontSize="20" />
+                            <Text ml={4} fontWeight="medium">Suporte</Text>
                         </Link>
                     </Stack>
                 </Box>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react"
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
@@ -8,6 +8,27 @@ const Dashboard = () => {
             <Header />
             <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                    >
+                        <Text fontSize="lg" mb="4">
+                            Temperatura
+                        </Text>
+                    </Box>
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                    >
+                        <Text fontSize="lg" mb="4">
+                            Umidade
+                        </Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     );
